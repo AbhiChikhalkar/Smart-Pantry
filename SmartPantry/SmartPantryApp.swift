8bda2cb3891de1ea85c775a76d2f5ea3bbc32684
Abhishek Chikhalkar <abhichikhalkar12399@gmail.com>
2025-12-03 17:46:08 +0100

    Initial Commit

---
+++ SmartPantry/SmartPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPantryApp.swift
+//  SmartPantry
+//
+//  Created by Abhishek Chikhalkar on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
